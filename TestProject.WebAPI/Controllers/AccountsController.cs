0612372e2ed3438b1b8af3c27b0c121bffab70dd
Atul Chinchalkar <atulsc2010@gmail.com>
2020-09-30 00:14:06 +1000

    downgraded to net3.0

--- TestProject.WebAPI/Controllers/AccountsController.cs
+++ TestProject.WebAPI/Controllers/AccountsController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using TestProject.WebAPI.Commands.Accounts;
-using TestProject.WebAPI.Domain;
 using TestProject.WebAPI.Queries.Accounts;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -22,11 +18,6 @@ namespace TestProject.WebAPI.Controllers
             var response = await Mediator.Send(query);
 
             return Ok(response);
-
-            //return new List<Account> 
-            //{   new Account { Id = Guid.NewGuid() },
-            //    new Account { Id = Guid.NewGuid() }
-            //};
         }
 
         [HttpPost("create")]
