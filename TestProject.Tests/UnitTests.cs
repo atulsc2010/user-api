6376716693ffae66e5a9298b92efd361b6088993
Atul Chinchalkar <atulsc2010@gmail.com>
2020-09-30 12:37:47 +1000

    Tests added for business rules

--- TestProject.Tests/UnitTests.cs
+++ TestProject.Tests/UnitTests.cs
@@ -10,13 +10,16 @@ using Microsoft.EntityFrameworkCore;
 using TestProject.WebAPI.Models;
 using TestProject.WebAPI.Domain;
 using System.Collections.Generic;
+using TestProject.WebAPI.Commands.Users;
+using TestProject.WebAPI.Commands.Accounts;
+using TestProject.WebAPI.Queries.Accounts;
 
 namespace TestProject.Tests
 {
     public class UnitTests
     {
         [Fact]
-        public async Task GetUserQueryHandler_returns_a_user_when_available()
+        public async Task GetUserHandler_returns_a_user_when_available()
         {
 
             //Arrange
@@ -48,7 +51,7 @@ namespace TestProject.Tests
         }
 
         [Fact]
-        public async Task GetUserQueryHandler_does_not_return_a_user_when_not_available()
+        public async Task GetUserQuery_does_not_return_a_user_when_not_available()
         {
 
             //Arrange
@@ -80,7 +83,7 @@ namespace TestProject.Tests
         }
 
         [Fact]
-        public async Task ListUsersQueryHandler_returns_a_list_of_users_when_available()
+        public async Task ListUsersQuery_returns_a_list_of_users_when_available()
         {
             //Arrange
             var userId1 = Guid.NewGuid();
@@ -111,7 +114,7 @@ namespace TestProject.Tests
         }
 
         [Fact]
-        public async Task ListUsersQueryHandler_returns_a_blanklist_when_table_is_empty()
+        public async Task ListUsersQuery_returns_a_blanklist_when_table_is_empty()
         {
             //Arrange
             IEnumerable<ListUsersResponse> response;
@@ -131,6 +134,233 @@ namespace TestProject.Tests
             //Assert
             response.Count().Should().Be(0);
         }
+
+        [Fact]
+        public async Task CreateUserCommand_creates_user_when_request_is_valid()
+        {
+            //Arrange
+            var request = new CreateUserRequest { Name = "test", Email = "test@email.com", Salary = 2000, Expenses = 500 };
+            var response = new CreateUserResponse();
+
+            using (var db = MockDbContext())
+            {
+                db.Users.RemoveRange(db.Users);
+                db.SaveChanges();
+
+                var command = new CreateUserCommand(request);
+                var handler = new CreateUserCommandHandler(db);
+
+                //Act
+                response = await handler.Handle(command, CancellationToken.None);
+            }
+
+            //Assert
+            response.Status.Should().Be("Success");
+            response.Id.Should().NotBeEmpty();
+        }
+
+
+        [Fact]
+        public async Task CreateUserCommand_DoesNot_create_user_when_Salary_income_is_less_than_zero()
+        {
+            //Arrange
+            var request = new CreateUserRequest { Name = "test", Email = "test@email.com", Salary = -2000, Expenses = 2000 };
+            var response = new CreateUserResponse();
+
+            using (var db = MockDbContext())
+            {
+                db.Users.RemoveRange(db.Users);
+                db.SaveChanges();
+
+                var command = new CreateUserCommand(request);
+                var handler = new CreateUserCommandHandler(db);
+
+                //Act
+                response = await handler.Handle(command, CancellationToken.None);
+            }
+
+            //Assert
+            response.Status.Should().Contain("User cannot be created");
+            response.Id.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task CreateUserCommand_DoesNot_create_user_when_name_or_email_not_provided()
+        {
+            //Arrange
+            var request = new CreateUserRequest { Name = "test", Email = "", Salary = 2000, Expenses = 2000 };
+            var response = new CreateUserResponse();
+
+            using (var db = MockDbContext())
+            {
+                db.Users.RemoveRange(db.Users);
+                db.SaveChanges();
+
+                var command = new CreateUserCommand(request);
+                var handler = new CreateUserCommandHandler(db);
+
+                //Act
+                response = await handler.Handle(command, CancellationToken.None);
+            }
+
+            //Assert
+            response.Status.Should().Contain("User cannot be created");
+            response.Id.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task CreateUserCommand_DoesNot_create_user_when_email_exists_in_users_db()
+        {
+            //Arrange
+            var request = new CreateUserRequest { Name = "test", Email = "test@email.com", Salary = 2000, Expenses = 2000 };
+            var response = new CreateUserResponse();
+
+            using (var db = MockDbContext())
+            {
+                db.Users.RemoveRange(db.Users);
+                db.SaveChanges();
+
+                var command = new CreateUserCommand(request);
+                var handler = new CreateUserCommandHandler(db);
+
+                //Act
+                response = await handler.Handle(command, CancellationToken.None);
+            }
+
+            //Assert
+            response.Status.Should().Contain("Success");
+            response.Id.Should().NotBeEmpty();
+
+            //Arrange 2
+            request = new CreateUserRequest { Name = "new user", Email = "test@email.com", Salary = 3000, Expenses = 500 };
+            response = new CreateUserResponse();
+
+            using (var db = MockDbContext())
+            {
+                var command = new CreateUserCommand(request);
+                var handler = new CreateUserCommandHandler(db);
+
+                //Act
+                response = await handler.Handle(command, CancellationToken.None);
+            }
+
+            //Assert
+            response.Status.Should().Contain("User cannot be created");
+            response.Id.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task CreateAccountCommand_creates_account_when_request_is_valid_and_User_exists()
+        {
+            //Arrange
+            var userId = Guid.NewGuid();
+            var request = new CreateAccountRequest { UserId = userId };
+            var response = new CreateAccountResponse();
+
+            using (var db = MockDbContext())
+            {
+                db.Users.RemoveRange(db.Users);
+                db.SaveChanges();
+
+                db.Users.Add(new User { Id = userId, Name = "Tester1", Email = "email1@email.com", Salary = 2000 , Expenses = 1000 });
+                db.SaveChanges();
+
+
+                var command = new CreateAccountCommand(request);
+                var handler = new CreateAccountCommandHandler(db);
+
+                //Act
+                response = await handler.Handle(command, CancellationToken.None);
+            }
+
+            //Assert
+            response.Status.Should().Be("Success");
+            response.Id.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task CreateAccountCommand_DoesNot_create_account_when_User_notfound()
+        {
+            //Arrange
+            var userId = Guid.NewGuid();
+            var request = new CreateAccountRequest { UserId = userId };
+            var response = new CreateAccountResponse();
+
+            using (var db = MockDbContext())
+            {
+                db.Users.RemoveRange(db.Users);
+                db.SaveChanges();
+
+                var command = new CreateAccountCommand(request);
+                var handler = new CreateAccountCommandHandler(db);
+
+                //Act
+                response = await handler.Handle(command, CancellationToken.None);
+            }
+
+            //Assert
+            response.Status.Should().Contain("not be created");
+            response.Id.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task CreateAccountCommand_DoesNot_create_account_when_Income_threshold_is_not_met()
+        {
+            //Arrange
+            var userId = Guid.NewGuid();
+            var request = new CreateAccountRequest { UserId = userId };
+            var response = new CreateAccountResponse();
+
+            using (var db = MockDbContext())
+            {
+                db.Users.RemoveRange(db.Users);
+                db.SaveChanges();
+
+                db.Users.Add(new User { Id = userId, Name = "Tester1", Email = "email1@email.com", Salary = 2000, Expenses = 1500 });
+                db.SaveChanges();
+
+                var command = new CreateAccountCommand(request);
+                var handler = new CreateAccountCommandHandler(db);
+
+                //Act
+                response = await handler.Handle(command, CancellationToken.None);
+            }
+
+            //Assert
+            response.Status.Should().Contain("Income");
+            response.Id.Should().BeEmpty();
+        }
+
+
+        [Fact]
+        public async Task ListAccountsQuery_returns_a_list_of_accounts_when_available()
+        {
+            //Arrange
+
+            IEnumerable<ListAccountsResponse> response;
+
+            using (var db = MockDbContext())
+            {
+                db.Accounts.RemoveRange(db.Accounts);
+                db.Accounts.Add(new Account { Id = Guid.NewGuid(), Name = "account1", Status = AccountStatus.Active });
+                db.Accounts.Add(new Account { Id = Guid.NewGuid(), Name = "account2", Status = AccountStatus.Active });
+                db.Accounts.Add(new Account { Id = Guid.NewGuid(), Name = "account3", Status = AccountStatus.Active });
+                db.SaveChanges();
+            }
+
+            using (var db = MockDbContext())
+            {
+                var request = new ListAccountsQuery();
+                var handler = new ListAccountsQueryHandler(db);
+
+                //Act
+                response = await handler.Handle(request, CancellationToken.None);
+            }
+
+            //Assert
+            response.Count().Should().Be(3);
+        }
+
 
         private static ApiDbContext MockDbContext()
         {
